d1e117ef85eb164e1282790f6e098d166b261164
Rafael Alvarez <rafaelalvarez3@proton.me>
2025-02-04 21:34:22 -0600

    more mapping yay.

--- Sources/Kernel/MiniUART.swift
+++ Sources/Kernel/MiniUART.swift
@@ -48,17 +48,17 @@ struct MiniUART {
 @Register(bitWidth: 32)
 struct AUX_IRQ {
     
-    @ReadOnly(bits: 0..<1, as: Bool.self)
-    var mini_uart_irq: MINI_UART_IRQ
-    
-    @ReadOnly(bits: 1..<2, as: Bool.self)
-    var spi_one_irq: SPI_ONE_IRQ
-    
-    @ReadOnly(bits: 2..<3, as: Bool.self)
-    var spi_two_irq: SPI_TWO_IRQ
-    
     @Reserved(bits: 3..<32)
     
+    @ReadOnly(bits: 2..<3)
+    var spi_two_irq: SPI_TWO_IRQ
+    
+    @ReadOnly(bits: 1..<2)
+    var spi_one_irq: SPI_ONE_IRQ
+    
+    @ReadOnly(bits: 0..<1)
+    var mini_uart_irq: MINI_UART_IRQ
+
 }
 
 /* ---------------------------------------------------------------------------------- */
@@ -66,22 +66,78 @@ struct AUX_IRQ {
 @Register(bitWidth: 32)
 struct AUX_ENABLES {
     
-    @ReadWrite(bits: 0..<1, as: Bool.self)
-    var mini_uart_enable: MINI_UART_ENABLE
-    
-    @ReadWrite(bits: 1..<2, as: Bool.self)
-    var spi_one_enable: SPI_ONE_ENABLE
+    @Reserved(bits: 3..<32)
     
     @ReadWrite(bits: 2..<3, as: Bool.self)
     var spi_two_enable: SPI_TWO_ENABLE
     
-    @Reserved(bits: 3..<32)
+    @ReadWrite(bits: 1..<2, as: Bool.self)
+    var spi_one_enable: SPI_ONE_ENABLE
+    
+    @ReadWrite(bits: 0..<1, as: Bool.self)
+    var mini_uart_enable: MINI_UART_ENABLE
+
+}
+
+/* ---------------------------------------------------------------------------------- */
+
+@Register(bitWidth: 32)
+struct AUX_MU_IO_REG {
+    
+    @Reserved(bits: 8..<32)
+    
+    @ReadWrite(bits: 0..<8)
+    var ls_eight_bits_baudrate: LS_EIGHT_BITS_BAUDRATE
+    
+    @WriteOnly(bits: 0..<8)
+    var transmit_data_write: TRANSMIT_DATA_WRITE
+    
+    @ReadOnly(bits: 0..<8)
+    var receive_data_read: RECEIVE_DATA_READ
     
 }
 
 /* ---------------------------------------------------------------------------------- */
 
 @Register(bitWidth: 32)
+struct AUX_MU_IER_REG {
+    
+    @Reserved(bits: 8..<32)
+    
+    @ReadWrite(bits: 0..<8)
+    var ms_eight_bits_baudrate: MS_EIGHT_BITS_BAUDRATE
+    
+    @ReadWrite(bits: 1..<2, as: Bool.self)
+    var enable_receive_interrupt: ENABLE_RECEIVE_INTERRUPT
+    
+    @ReadWrite(bits: 0..<1, as: Bool.self)
+    var enable_receive_interrupt: ENABLE_RECEIVE_INTERRUPT
+    
+}
+
+/* ---------------------------------------------------------------------------------- */
+
+@Register(bitWidth: 32)
+struct AUX_MU_IIR_REG {
+    // map bits
+}
+
+/* ---------------------------------------------------------------------------------- */
+
+@Register(bitWidth: 32)
 struct AUX_MU_LCR_REG {
+    
+    @Reserved(bits: 8..<32)
+    
+    @ReadWrite(bits: 6..<7, as: Bool.self)
+    var break_condition: BREAK_CONDITION
+    
+    @ReadWrite(bits: 7..<8, as: Bool.self)
+    var dlab_access: DLAB_ACCESS
+    
+    @Reserved(bits: 1..<6)
+    
+    @ReadWrite(bits: 0..<1, as: Bool.self)
+    var data_size: DATA_SIZE
     
 }
